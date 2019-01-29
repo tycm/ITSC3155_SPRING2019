01d3abefa9caca2fbe6a58517ccfdc449a02ef51
tmccrick <tmccrick@uncc.edu>
2019-01-29 15:43:48 +0000

    RubyBasics1 Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,20 @@
+# Lab 1
+# Part I
+def sum arr
+  sum = 0
+
+  for hold in arr do
+    sum += hold
+  end
+  return sum
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
