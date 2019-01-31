e858f0bf8f0bef8e4c7c4460801f9eec1d7d1310
tmccrick <tmccrick@uncc.edu>
2019-01-31 15:25:45 +0000

    RubyBasics1 - Part3 V2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -40,20 +40,24 @@ end
 
 # Part III
 def sum_to_n? arr, n
-  if arr.length == 0
+  if (arr.length == 0)
     return false
   end
-  if arr.length == 1
+  if (arr.length == 1)
     return false
   end
-  for outer in arr
-    for inner in arr
-      if outer != inner
-        hold = outer + inner
-      end
-      if hold == n
-        return true
-      end
+  sorted_arr = arr.sort
+  head =0
+  tail = sorted_arr.length-1
+  
+  while head < tail 
+    current_sum = sorted_arr[head] + sorted_arr[tail]
+    if current_sum == n
+      return true
+    elsif current_sum < n
+      head = head + 1
+    else 
+      tail = tail - 1
     end
   end
   return false
