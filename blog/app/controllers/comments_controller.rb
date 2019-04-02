4396ae01e9c37d53cc70ca839d0f29780f77550d
tmccrick <tmccrick@uncc.edu>
2019-04-02 14:35:02 +0000

    Add comments to the show view

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
     def create
-        @article = Artile.find(params[:article_id])
+        @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
     end
